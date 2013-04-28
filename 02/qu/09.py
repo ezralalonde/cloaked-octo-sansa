5033d8e5d36c569504f4565c06e507518dc16b4f
Ezra Lalonde <ezrais@gmail.com>
2013-04-28 13:57:49 -0600

    Create 09.py

---
+++ 02/qu/09.py
@@ -0,0 +1,18 @@
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints 
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+
+def print_numbers(nn):
+    ii = 0
+    while ii < nn:
+        ii = ii + 1
+        print ii
+
+#print_numbers(3)
+#>>> 1
+#>>> 2
+#>>> 3
