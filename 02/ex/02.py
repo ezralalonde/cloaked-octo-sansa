8eff29ba9777cd977f04a2c0b68d598ad63c8f47
Ezra Lalonde <ezrais@gmail.com>
2013-04-28 15:13:04 -0600

    Create 02.py

---
+++ 02/ex/02.py
@@ -0,0 +1,17 @@
+# Define a procedure, stamps, which takes as its input a positive integer in
+# pence and returns the number of 5p, 2p and 1p stamps (p is pence) required 
+# to make up that value. The answer should use as many 5p stamps as possible,
+# then 2 pence stamps and finally 1p stamps.
+
+def stamps(nn):
+    # Your code here
+    return (nn / 5, (nn % 5) / 2, (nn % 5) % 2) 
+
+print stamps(8)
+#>>> (1, 1, 1)  # one 5p stamp, one 2p stamp and one 1p stamp
+print stamps(5)
+#>>> (1, 0, 0)  # one 5p stamp, no 2p stamps and no 1p stamps
+print stamps(29)
+#>>> (5, 2, 0)  # five 5p stamps, two 2p stamps and no 1p stamps
+print stamps(0)
+#>>> (0, 0, 0) # no 5p stamps, no 2p stamps and no 1p stamps
