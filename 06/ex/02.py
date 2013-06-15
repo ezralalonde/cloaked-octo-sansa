590d7bdea38c6a7f4d1390044fb7103dd4cc7591
ezralalonde <ezra@usefuliftrue.com>
2013-06-15 14:31:29 -0600

    double star; 02

---
+++ 06/ex/02.py
@@ -0,0 +1,61 @@
+# Double Gold Star
+
+# Khayyam Triangle
+
+# The French mathematician, Blaise Pascal, who built a mechanical computer in
+# the 17th century, studied a pattern of numbers now commonly known in parts of
+# the world as Pascal's Triangle (it was also previously studied by many Indian,
+# Chinese, and Persian mathematicians, and is known by different names in other
+# parts of the world).
+
+# The pattern is shown below:
+
+#                    1
+#                   1 1
+#                  1 2 1
+#                 1 3 3 1
+#                1 4 6 4 1
+#                   ...
+
+# Each number is the sum of the number above it to the left and the number above
+# it to the right (any missing numbers are counted as 0).
+
+# Define a procedure, triangle(n), that takes a number n as its input, and
+# returns a list of the first n rows in the triangle. Each element of the
+# returned list should be a list of the numbers at the corresponding row in the
+# triangle.
+
+def triangle(nn):
+    if nn == 0:
+        return []
+    if nn == 1:
+        return [[1]]
+    tri = triangle(nn-1)
+    return tri + [add(tri[-1])]
+
+def add(xs):
+    out = []
+    for ii in zip(xs, [0]+xs) + [(0,1)]:
+        out.append(sum(ii))
+    return out
+
+print add([])
+print add([1])
+print add([1,1])
+print add([1,2,1])
+#For example:
+
+print triangle(0)
+#>>> []
+
+print triangle(1)
+#>>> [[1]]
+
+print triangle(2)
+#>> [[1], [1, 1]]
+
+print triangle(3)
+#>>> [[1], [1, 1], [1, 2, 1]]
+
+print triangle(6)
+#>>> [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]
